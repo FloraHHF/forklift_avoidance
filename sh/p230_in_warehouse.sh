d5e4f815015febc8a5989d4da7bc90cfc57b469f
flora <flora.fu@mail.utoronto.ca>
2023-01-24 11:31:20 -0500

    run through p230 simulation with event camera plugin

--- sh/p230_in_warehouse.sh
+++ sh/p230_in_warehouse.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # This scripts launches a P230 model in warehouse
 
-# roscore
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+# TODO:
+# 1. launch by  "prometheus_gazebo   sitl_indoor_1uav.launch" with arguments,
+# 	 instead of "forklift_avoidance  sitl_p230.launch
+# 2. launch by  "roslaunch prometheus_uav_control  uav_control_main_outdoor.launch" with arguments,
+#    instead of "forklift_avoidance                uav_controller.launch"
+
+gnome-terminal \
+--window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 5; roslaunch forklift_avoidance sitl_p230.launch; exec bash"' \
 --tab -e 'bash -c "sleep 6; roslaunch forklift_avoidance uav_controller.launch; exec bash"' \
 --tab -e 'bash -c "sleep 7; rosrun forklift_avoidance takeoff_land.py; exec bash"' \
+--tab -e 'bash -c "sleep 8; roslaunch forklift_avoidance davis_render.launch; exec bash"' \
